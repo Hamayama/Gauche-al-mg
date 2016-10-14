32b4b21a044671f932566c1174510aa88bbe8958
Hamayama <hamay1010@gmail.com>
2016-10-14 12:53:49 +0900

    example/mml chg

--- example/mml/README.md
+++ example/mml/README.md
@@ -2,7 +2,7 @@
 
 ## 概要
 - Gauche-al を用いて、  
-  MML(Music Macro Language)による音楽演奏を行うサンプルです。
+  MML (Music Macro Language) による音楽演奏を行うサンプルです。
 
 
 ## 環境のインストール方法
@@ -15,15 +15,15 @@
 
 ## 実行方法
 - test1001.bat をダブルクリック等で起動します。  
-  (test1001.bat, test1001.scm, mmlproc.scm が同一フォルダに存在する必要があります)  
   しばらくすると音楽が鳴ります。演奏が終わると終了します。
 
 
 ## ファイルについて
-- test1001.scm の25行目の「 (set! pcm-raw (mml->pcm 」の下がMMLの文字列になっています。  
+- test1001.scm の25行目の「 ($ set! pcm-raw $ mml->pcm $ string-append 」の下が、  
+  MML の文字列になっています。  
   ここを書き換えることで、いろいろな音楽を演奏できます。
 
-- MMLの解釈は mmlproc.scm というモジュールで行っています。  
+- MML の解釈は mmlproc.scm というモジュールで行っています。  
   mmlproc の詳細については、以下のページを参照ください。  
   https://github.com/Hamayama/mmlproc
 
@@ -33,12 +33,12 @@
   - Windows 8.1 (64bit)
   - Windows XP Home SP3
 - 環境
-  - MinGW (32bit) (gcc v4.8.1)
   - MSYS2/MinGW-w64 (64bit) (gcc version 6.2.0 (Rev2, Built by MSYS2 project))
   - MSYS2/MinGW-w64 (32bit) (gcc version 6.2.0 (Rev2, Built by MSYS2 project))
+  - MinGW (32bit) (gcc v4.8.1)
 - 言語
-  - Gauche v0.9.4
   - Gauche v0.9.5
+  - Gauche v0.9.4
 
 ## 履歴
 - 2015-12-24  v1.00 初版
@@ -50,6 +50,7 @@
 - 2016-3-31   v1.05 test1001.scmの演奏終了チェック処理見直し
 - 2016-4-17   v1.06 mmlproc.scmをv1.16に更新
 - 2016-10-12  v1.07 README修正のみ(Gauche v0.9.5 対応)
+- 2016-10-14  v1.08 test1001.scmのMML文字列読み込み処理見直し等
 
 
-(2016-10-12)
+(2016-10-14)
--- example/mml/test1001.scm
+++ example/mml/test1001.scm
@@ -1,7 +1,7 @@
 ;; -*- coding: utf-8 -*-
 ;;
 ;; MML演奏テスト
-;; 2016-4-17
+;; 2016-10-14
 ;;
 (add-load-path "." :relative)
 (use al)
@@ -12,9 +12,9 @@
 ;; 変数
 (define pcm-raw  #f) ; PCMの生データ
 (define wav-data #f) ; WAVデータ
-(define buf #f) ; Gauche-alのバッファ情報
-(define pcm #f) ; Gauche-alのPCMデータ情報
-(define src #f) ; Gauche-alのソース情報
+(define buf #f)      ; Gauche-alのバッファ情報
+(define pcm #f)      ; Gauche-alのPCMデータ情報
+(define src #f)      ; Gauche-alのソース情報
 
 ;; 初期化
 (set! mml-sample-rate 44100)
@@ -22,13 +22,13 @@
 
 ;; MML文字列を読み込んでWAVデータに変換
 (print "mml->wav")
-(set! pcm-raw (mml->pcm (string-append
-  "!c0 @500 o4 "
-  "  d8b8a8g8d2 | d8b8a8g8e2      | e8>c8<b8a8f+2       | >d8d8c8<a8b2  "
-  "  d8b8a8g8d2 | d8b8a8g8e2      | e8>c8<b8a8>d8d8d8d8 | e8d8c8<a8g>d< "
-  "  b8b8bb8b8b | b8>d8<g8.a16b2> | c8c8c8c8c8<b8b8b8   | b8a8a8g8a>d<  "
-  "  b8b8bb8b8b | b8>d8<g8.a16b2> | c8c8c8c8c8<b8b8b8   | >d8d8c8<a8g2  "
-  )))
+($ set! pcm-raw $ mml->pcm $ string-append
+   "!c0 @500 o4 "
+   "  d8b8a8g8d2 | d8b8a8g8e2      | e8>c8<b8a8f+2       | >d8d8c8<a8b2  "
+   "  d8b8a8g8d2 | d8b8a8g8e2      | e8>c8<b8a8>d8d8d8d8 | e8d8c8<a8g>d< "
+   "  b8b8bb8b8b | b8>d8<g8.a16b2> | c8c8c8c8c8<b8b8b8   | b8a8a8g8a>d<  "
+   "  b8b8bb8b8b | b8>d8<g8.a16b2> | c8c8c8c8c8<b8b8b8   | >d8d8c8<a8g2  "
+   )
 (set! wav-data (make-u8vector (get-wav-size pcm-raw)))
 (let1 out (open-output-uvector wav-data)
   (unwind-protect
